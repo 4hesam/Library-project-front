f940e061b8c7a6d31687dd5bbbf5c6447c5b1bf1
hesam <4hesam0@gmail.com>
2025-11-27 16:02:40 +0330

    feet: add header

---
+++ src/components/headerPage.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="header-all">
+    <!-- $q.screen.lt.xs -->
+    <img src="/src/assets/logo library black.jpg" class="logo" />
+    <div class="short-tag">
+      <p>Home</p>
+      <p>E-book</p>
+      <p>Audiobooks</p>
+      <p>Free Content</p>
+      <p>Authors</p>
+      <p style="color: crimson">Premium Library</p>
+    </div>
+    <div class="search-box">
+      <CusSearchBar :debounce="500" icon="search" @search="searchHandler" class="search-ico" />
+    </div>
+    <div class="btn-head">
+      <CusButton outline color="LightGray	" icon="shopping_cart" class="shop-ico" />
+      <CusButton color="red" label="Login" class="log-ico" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import CusSearchBar from 'src/components/molecules/CustomSearchBar.vue'
+import CusButton from 'src/components/molecules/CustomButton.vue'
+</script>
+
+<style>
+.header-all {
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  margin: 5px;
+  box-shadow: 0px 2px 4px black;
+  border-radius: 10px;
+}
+.logo {
+  height: 40px;
+  border-radius: 50px;
+}
+.short-tag {
+  display: flex;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  flex: 1;
+  gap: 10px;
+  margin-left: 10px;
+  padding-top: 10px;
+}
+.search-box {
+  margin-left: auto;
+  display: flex;
+  align-items: center;
+
+  /* width: 100%; */
+}
+.btn-head {
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  margin-left: 10px;
+}
+.shop-ico {
+  border-radius: 5px;
+  width: 40px;
+  box-shadow: 0.5px 2px 4px black;
+}
+.log-ico {
+  box-shadow: 0px 0.5px 4px black;
+}
+</style>
---
+++ src/components/molecules/CustomButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-btn
+    :label="label"
+    :color="color"
+    :flat="flat"
+    :outline="outline"
+    :size="size"
+    :icon="icon"
+    @click="handleClick"
+  />
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue'
+import { QBtn } from 'quasar'
+
+// تعریف props به صورت مستقیم بدون ایجاد متغیر اضافی
+defineProps({
+  label: { type: String, required: true },
+  color: { type: String, default: 'blue' },
+  flat: { type: Boolean, default: false },
+  outline: { type: Boolean, default: false },
+  size: { type: String, default: 'md' },
+  icon: { type: String},
+})
+
+// تعریف emit برای رویداد کلیک
+const emit = defineEmits(['click'])
+
+// تابع برای emit کردن رویداد کلیک
+function handleClick() {
+  emit('click')
+}
+</script>
